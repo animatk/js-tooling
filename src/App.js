350511c9316268f6c9549524431ab4699392e08a
Diego Santos <diego.santos@prodigious.com>
2020-01-09 20:46:45 -0500

    Add Babel support to class proposed syntax, add sample on App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
+    state = {
+        count: 0
+    }
     render() {
-        return <h1>Hello World!</h1>
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <h2>count: {this.state.count}</h2>
+                <button onClick={() => this.setState({count: this.state.count + 1})}> + </button>
+                <button onClick={() => this.setState({count: this.state.count - 1})}> - </button>
+            </div>
+        )
     }
 }
 
